a080149c5a70117613244a36d223610a4c8a95b2
sumanw <wadhwani.suman@gmail.com>
2020-02-14 15:37:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# objectprototype
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/objectprototype)
---
+++ index.js
@@ -0,0 +1,72 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+var Person = function(name) {
+  this.name = name;
+  this.canTalk = true;
+};
+
+Person.prototype.greet = function() {
+  if (this.canTalk) {
+    return ('Hi, I am ' + this.name);
+  }
+};
+
+var Employee = function(name, title) {
+  Person.call(this, name);
+  this.title = title;
+};
+
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee; //If you don't set Object.prototype.constructor to Employee, 
+                                           //it will take prototype.constructor of Person (parent). 
+                                           //To avoid that, we set the prototype.constructor to Employee (child).
+
+
+Employee.prototype.greet = function() {
+  if (this.canTalk) {
+    return  ('Hi, I am ' + this.name + ', the ' + this.title);
+  }
+};
+
+var Customer = function(name) {
+  Person.call(this, name);
+};
+
+Customer.prototype = Object.create(Person.prototype);
+Customer.prototype.constructor = Customer; //If you don't set Object.prototype.constructor to Customer, 
+                                           //it will take prototype.constructor of Person (parent). 
+                                           //To avoid that, we set the prototype.constructor to Customer (child).
+
+
+var Mime = function(name) {
+  Person.call(this, name);
+  this.canTalk = false;
+};
+
+Mime.prototype = Object.create(Person.prototype);
+Mime.prototype.constructor = Mime; //If you don't set Object.prototype.constructor to Mime,
+                                   //it will take prototype.constructor of Person (parent).
+                                   //To avoid that, we set the prototype.constructor to Mime (child).
+
+
+var bob = new Employee('Bob', 'Builder');
+console.log(bob);
+var joe = new Customer('Joe');
+var rg = new Employee('Red Green', 'Handyman');
+var mike = new Customer('Mike');
+var mime = new Mime('Mime');
+var htmlDisplay="hello ";
+htmlDisplay=htmlDisplay + bob.greet() +"<br/>";
+// Hi, I am Bob, the Builder
+htmlDisplay=htmlDisplay + joe.greet() +"<br/>";
+// Hi, I am Joe
+rg.greet();
+// Hi, I am Red Green, the Handyman
+mike.greet();
+// Hi, I am Mike
+mime.greet();
+appDiv.innerHTML = htmlDisplay;
+
